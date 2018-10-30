9f0f8ef29e1a00ddf173da14e0d9ee8028c79822
tpps88206 <tpps88206@yahoo.com.tw>
2018-10-30 18:58:10 +0800

    動態產生多個 React 元件

--- TodoApp/TodoApp.js
+++ TodoApp/TodoApp.js
@@ -4,6 +4,29 @@ const {
     TodoList
 } = window.App;
 
+const todos = [
+    {
+        id: 0,
+        title: 'Item 1',
+        completed: false
+    },
+    {
+        id: 1,
+        title: 'Item 2',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'Item 3',
+        completed: false
+    },
+    {
+        id: 3,
+        title: 'Item 4',
+        completed: false
+    }
+];
+
 class TodoApp extends React.Component {
     render() {
         return (
@@ -11,10 +34,10 @@ class TodoApp extends React.Component {
                 <TodoHeader
                     title="我的待辦清單"
                     username="Jason"
-                    todoCount={99}
+                    todoCount={todos.filter((todo) => !todo.completed).length}
                 />
                 <InputField placeholder="新增待辦清單" />
-                <TodoList />
+                <TodoList todos={todos} />
             </div>
         );
     }
--- TodoApp/TodoList.js
+++ TodoApp/TodoList.js
@@ -2,29 +2,21 @@ const { TodoItem } = window.App;
 
 class TodoList extends React.Component {
     render() {
-        return (
-            <ul>
-                <li>
-                    <TodoItem
-                        title="Item 1"
-                        completed={false}
-                    />
-                </li>
-                <li>
-                    <TodoItem
-                        title="Item 2"
-                        completed={false}
-                    />
-                </li>
-                <li>
-                    <TodoItem
-                        title="Item 3"
-                        completed={false}
-                    />
-                </li>
-            </ul>
-        );
+        const { todos } = this.props;
+        const todoElements = todos.map((todo) => (
+            <li key={todo.id}>
+                <TodoItem
+                    title={todo.title}
+                    completed={todo.completed}
+                />
+            </li>
+        ));
+        return <ul>{todoElements}</ul>;
     }
 }
+
+TodoList.propTypes = {
+    todos: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
+};
 
 window.App.TodoList = TodoList;
